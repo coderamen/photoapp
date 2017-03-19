f1c7ecd9354a7b26fad8af4ce0deba78277eb332
Rails <spencerteh@gmail.com>
2017-03-19 15:57:14 +0800

    reset create_user table, fix migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170319065316) do
+ActiveRecord::Schema.define(version: 20170319073824) do
 
-  create_table "comments", force: :cascade do |t|
-    t.text     "description"
-    t.integer  "user_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.index ["user_id"], name: "index_comments_on_user_id"
-  end
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
