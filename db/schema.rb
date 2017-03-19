f290288c3c771c372327fac4af7eb7b7a35c0709
Rails <spencerteh@gmail.com>
2017-03-19 20:22:19 +0800

    1. Install Bootstrap, basic navbar and dead links 2. Setup Pics controller, view files

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170319073824) do
+ActiveRecord::Schema.define(version: 20170319120933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pics", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
