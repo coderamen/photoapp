729c417da7980f556713685b790d529989996713
Rails <spencerteh@gmail.com>
2017-03-20 17:33:29 +0800

    New User Sign Up, Edit User/NOT TESTED

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   resources :pics
+
+  get 'signup', to: 'users#new'
+  post 'user', to: 'users#create'
+  resources :users, except: [:new]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
