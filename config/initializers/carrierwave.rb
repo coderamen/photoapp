7d2fc3272f64f321b425c78bf98003eb2722533e
Rails <spencerteh@gmail.com>
2017-03-25 12:15:29 +0800

    add config_root and config_cache_dir to init/carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,6 +3,8 @@
 
 # require 'carrierwave/storage/fog'
   CarrierWave.configure do |config|
+    config.root = Rails.root.join('tmp')
+    config.cache_dir = 'carrierwave' 
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider: 'AWS',
